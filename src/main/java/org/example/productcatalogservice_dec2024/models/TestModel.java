6141673b7e6698967ec5ef4f6ac252e61980db7b
khannaanurag1 <khannaanurag1@gmail.com>
2024-12-23 22:15:22 +0530

    Db Schemas Generation

---
+++ src/main/java/org/example/productcatalogservice_dec2024/models/TestModel.java
@@ -0,0 +1,8 @@
+package org.example.productcatalogservice_dec2024.models;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class TestModel extends BaseModel {
+    private Integer numField;
+}
